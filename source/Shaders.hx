f00e554d6b01abbc307086535675be3e8cb44f07
Aaron Rasmus <66130669+voxovistired@users.noreply.github.com>
2023-04-18 16:02:51 -0500

    hard

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -147,6 +147,39 @@ class WiggleShader extends FlxShader
 	}
 }
 
+class ReflectionShader extends FlxShader
+{
+  @:glFragmentSource('
+  
+    #pragma header
+
+    uniform float reflectionY = 0.36;
+
+
+    vec4 color = vec4(1.0);
+    void main()
+    {
+      vec2 uv = openfl_TextureCoordv.xy / iResolution.xy;
+      if(uv.y <= reflectionY)
+      {
+        float oy = uv.y;
+        uv.y = 2.0 * reflectionY - uv.y;
+        color = vec4(0.7, 0.85, 1.0, 1.0);
+      }
+
+        gl_FragColor = flixel_texture2D(bitmap, uv) * Color;
+    }
+  
+  ')
+
+  public function new()
+  {
+    super();
+    reflectionY.value = [0.36];
+  }
+}
+
+
 //I'll find another shader soon
 class Pibbified extends FlxShader
 {
