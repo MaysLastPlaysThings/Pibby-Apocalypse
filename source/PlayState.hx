1f9604e932f0839c0b5abdf10e94331e6375fd1f
RequiemZeroGH <pelaezasturias1405@gmail.com>
2023-05-30 12:00:00 -0700

    sksksksksksksksks

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4949,7 +4949,7 @@ class PlayState extends MusicBeatState
 
 			// Rating FC
 			ratingFC = "";
-			if (sicks > 0) ratingFC = "SFC";
+			if (sicks > 0) ratingFC = "PFC";
 			if (goods > 0) ratingFC = "GFC";
 			if (bads > 0 || shits > 0) ratingFC = "FC";
 			if (songMisses > 0 && songMisses < 10) ratingFC = "SDCB";
