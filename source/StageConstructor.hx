c8581db60c1c8c8006ece6d2f2ba1f5fd8cd46e7
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2023-07-18 20:44:44 -0300

    mfs when the .alpha exist

--- source/StageConstructor.hx
+++ source/StageConstructor.hx
@@ -39,6 +39,7 @@ class StageConstructor extends FlxTypedGroup<FlxBasic>
         additionalParams.set('stage', this);
         additionalParams.set('foreground', foreground);
         additionalParams.set('PlayState', PlayState.instance);
+        additionalParams.set('song', PlayState.SONG.song);
         additionalParams.set('retrieveAsset', function(path : String, assetType : AssetType):Dynamic {
             switch (assetType) {
                 case IMAGE:
