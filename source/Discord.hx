b388200a92d622fae0bf1d222f7d07df37c2a703
Aaron Rasmus <66130669+voxovistired@users.noreply.github.com>
2023-07-22 20:31:22 -0500

    havent committed in forever lol

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "840025802841653248",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
