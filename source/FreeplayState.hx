cc918f74a43af6bcb380dab6e080f9bc32be2d62
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2023-08-09 21:42:07 -0300

    funi moving things

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -525,6 +525,11 @@ class FreeplayState extends MusicBeatState
         }
 
 		Conductor.bpm = 100; // in case the code sucks and stays with the bpm
+
+		stagebox.y = 3 + Math.sin(Conductor.songPosition/600)*((FlxG.height * 0.015));
+		image.y = 3 + Math.sin(Conductor.songPosition/600)*((FlxG.height * 0.015));
+		arrowL.y = 290 + Math.sin(Conductor.songPosition/600)*((FlxG.height * 0.065));
+		arrowR.y = 290 + Math.sin(Conductor.songPosition/600)*((FlxG.height * 0.065));
 	}
 
 	override function beatHit() {
