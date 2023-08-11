b68da24cb56697004f011e3d98ca3b64bc251fd0
schweizer456 <108955170+schweizer456@users.noreply.github.com>
2023-08-11 15:39:27 +0300

    Revert "Merge branch 'main' of https://github.com/TormentedProgram/Pibby-Apocalypse"
    This reverts commit 968a492937381bddc91409654ab2eff74736eef5, reversing
    changes made to 5ba5040d542a11b23621a944ff3f3867a57af621.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1165,8 +1165,7 @@ class PlayState extends MusicBeatState
         FlxG.mouse.visible = false;
 
         if (gf != null) {
-			// for retcon cus yeah you get the point
-            iconP3.visible = (SONG.song == 'Retcon');
+            iconP3.visible = false;
         }
 		timeTxt.setFormat(Paths.font(storyWeekName + '.ttf'), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 
@@ -1883,7 +1882,7 @@ class PlayState extends MusicBeatState
 			if(scoreTxtTween != null) {
 				scoreTxtTween.cancel();
 			}
-			scoreTxt.angle = randomSeed.int(-2, 2, [-1, 0, 1]);
+			scoreTxt.angle = randomSeed.float(-2, 2);
 			scoreTxtTween = FlxTween.tween(scoreTxt, {angle: 0}, 0.2, {
 				onComplete: function(twn:FlxTween) {
 					scoreTxtTween = null;
@@ -5340,7 +5339,6 @@ class PlayState extends MusicBeatState
 							lyricTxt.text = "";
 							camGame.alpha = 1;
 							triggerEventNote('Apple Filter', 'on', 'white');
-							iconP3.visible = true;
 							if (ClientPrefs.flashing)
 								camOverlay.flash(FlxColor.WHITE, 1);
 							triggerEventNote('Cinematics', 'on', '1');
@@ -5437,7 +5435,6 @@ class PlayState extends MusicBeatState
 							if (ClientPrefs.flashing)
 								camOverlay.flash(FlxColor.WHITE, 1);
 							triggerEventNote('Cinematics', 'off', '1');
-							iconP3.visible = false;
 						case 2176:
 							changeChannel(2);
 							FlxTween.tween(camGame, {zoom: 1.1}, 0.00075, {
@@ -5572,8 +5569,6 @@ class PlayState extends MusicBeatState
 							defaultCamZoom = 0.7;
 							if (ClientPrefs.flashing)
 								camOverlay.flash(FlxColor.WHITE, 1);
-
-							iconP3.visible = true;
 
 							camHUD.setFilters([]);
 							camOverlay.setFilters([]);
@@ -6031,7 +6026,7 @@ class PlayState extends MusicBeatState
 							triggerEventNote('Camera Follow Pos', '1950', '1100');
 							gf.alpha = 0.0001;
 							jake.alpha = 0.0001;
-							theBlackness.alpha = 1;
+							theWhiteness.alpha = 1;
 							defaultCamZoom = 0.65;
 
 						case 2140:
@@ -6054,7 +6049,7 @@ class PlayState extends MusicBeatState
 							}
 							gf.alpha = 1;
 							jake.alpha = 1;
-							theBlackness.alpha = 0;
+							theWhiteness.alpha = 0;
 						case 2368:
 							FlxTween.tween(this, {abberationShaderIntensity: 0.1}, 2.67, {
 								ease: FlxEase.quadInOut,
@@ -6305,17 +6300,13 @@ class PlayState extends MusicBeatState
 					switch (curStep)
 					{
 						case 1:
-							if (noHeroIntro != null)
-								{
-									add(noHeroIntro);
-									noHeroIntro.animation.play('finnJumpscareMomento');
-								}
+							add(noHeroIntro);
+							noHeroIntro.animation.play('finnJumpscareMomento');
 
 						case 32: 
 							blackie.alpha = 0;
 							remove(noHeroIntro);
 							noHeroIntro.destroy();
-							noHeroIntro = null;
 							if (ClientPrefs.flashing) camGame.flash (FlxColor.WHITE, 1);
 							triggerEventNote('Cinematics', 'on', '1.3');
 							triggerEventNote('Apple Filter', 'on', 'white');
@@ -6511,11 +6502,6 @@ class PlayState extends MusicBeatState
 			{
 				jake.dance();
 			}}
-
-		@:privateAccess
-			{
-				if (curBeat % 20 == 0) Main.optimizeGame(false);
-			}
 
 		lastBeatHit = curBeat;
 
