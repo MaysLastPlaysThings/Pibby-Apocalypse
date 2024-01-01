5e30d292e58c0532f40e89bccc3b5e0cd20f470d
Random people <98900217+justaguy6@users.noreply.github.com>
2024-01-01 12:44:07 -0300

    Só otimzando o code

--- source/Song.hx
+++ source/Song.hx
@@ -4,12 +4,6 @@ import Section.SwagSection;
 import haxe.Json;
 import haxe.format.JsonParser;
 import lime.utils.Assets;
-
-#if sys
-import sys.io.File;
-import sys.FileSystem;
-#end
-
 using StringTools;
 
 typedef SwagSong =
@@ -92,19 +86,9 @@ class Song
 		
 		var formattedFolder:String = Paths.formatToSongPath(folder);
 		var formattedSong:String = Paths.formatToSongPath(jsonInput);
-		#if MODS_ALLOWED
-		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
-		if(FileSystem.exists(moddyFile)) {
-			rawJson = File.getContent(moddyFile).trim();
-		}
-		#end
-
+		
 		if(rawJson == null) {
-			#if sys
-			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
-			#else
-			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
-			#end
+			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();		
 		}
 
 		while (!rawJson.endsWith("}"))
