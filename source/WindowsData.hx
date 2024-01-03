ca52076993d173745d0fa4cbc3f1ec4af1d7eaa2
Random people <98900217+justaguy6@users.noreply.github.com>
2024-01-03 09:20:29 -0300

    Desaparece windows.h 2

--- source/WindowsData.hx
+++ source/WindowsData.hx
@@ -1,32 +1,9 @@
 package;
 
-//look at CppAPI.hx
-#if windows
-@:buildXml('
-<target id="haxe">
-    <lib name="dwmapi.lib" if="windows" />
-</target>
-')
-@:headerCode('
-#include <Windows.h>
-#include <cstdio>
-#include <iostream>
-#include <tchar.h>
-#include <dwmapi.h>
-#include <winuser.h>
-')
-#elseif android
+#if android
 @:headerCode("#include <stdio.h>")
-#end
 class WindowsData
-{
-	#if windows
-	@:functionCode("
-		unsigned long long allocatedRAM = 0;
-		GetPhysicallyInstalledSystemMemory(&allocatedRAM);
-		return (allocatedRAM / 1024);
-	")
-	#elseif android 
+{ 
 	@:functionCode('
 		FILE *meminfo = fopen("/proc/meminfo", "r");
 
@@ -47,78 +24,10 @@ class WindowsData
     	fclose(meminfo);
     	return -1;
 	')
-	#end
+	
 	public static function obtainRAM()
 	{
 		return 0;
 	}
-
-	#if windows
-	@:functionCode('
-        int darkMode = mode;
-        HWND window = GetActiveWindow();
-        if (S_OK != DwmSetWindowAttribute(window, 19, &darkMode, sizeof(darkMode))) {
-            DwmSetWindowAttribute(window, 20, &darkMode, sizeof(darkMode));
-        }
-        UpdateWindow(window);
-    ')
-	@:noCompletion
-	public static function _setWindowColorMode(mode:Int)
-	{
-	}
-
-	public static function setWindowColorMode(mode:WindowColorMode)
-	{
-		var darkMode:Int = cast(mode, Int);
-
-		if (darkMode > 1 || darkMode < 0)
-		{
-			trace("WindowColorMode Not Found...");
-
-			return;
-		}
-
-		_setWindowColorMode(darkMode);
-	}
-
-	@:functionCode('
-	HWND window = GetActiveWindow();
-	SetWindowLong(window, GWL_EXSTYLE, GetWindowLong(window, GWL_EXSTYLE) ^ WS_EX_LAYERED);
-	')
-	@:noCompletion
-	public static function _setWindowLayered()
-	{
-	}
-
-	@:functionCode('
-        HWND window = GetActiveWindow();
-
-		float a = alpha;
-
-		if (alpha > 1) {
-			a = 1;
-		} 
-		if (alpha < 0) {
-			a = 0;
-		}
-
-       	SetLayeredWindowAttributes(window, 0, (255 * (a * 100)) / 100, LWA_ALPHA);
-
-    ')
-	/**
-	 * Set Whole Window's Opacity
-	 * ! MAKE SURE TO CALL CppAPI._setWindowLayered(); BEFORE RUNNING THIS
-	 * @param alpha 
-	 */
-	public static function setWindowAlpha(alpha:Float)
-	{
-		return alpha;
-	}
 	#end
-}
-
-enum abstract WindowColorMode(Int)
-{
-	var DARK:WindowColorMode = 1;
-	var LIGHT:WindowColorMode = 0;
 }
