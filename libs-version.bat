e7263c343aaf1de8074081fc3eb1ec22bc5c89d3
MasterX <95196453+Default-name123@users.noreply.github.com>
2024-01-08 04:39:44 -0500

    Create libs-version.bat

---
+++ libs-version.bat
@@ -0,0 +1,9 @@
+haxelib set flixel 5.2.2
+haxelib set flixel-addons 2.11.0
+haxelib install flixel-tools
+haxelib install flixel-ui
+haxelib install hscript
+haxelib set hxCodec 2.6.1
+haxelib set openfl 9.2.1
+haxelib set lime 8.0.1
+haxelib set hxcpp 4.2.1
