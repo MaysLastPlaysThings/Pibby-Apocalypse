91919ac730f689ab718cc6299a7725411ac26125
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-01-13 09:49:13 +0200

    update source/Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -187,7 +187,7 @@ class Paths
 
 	static public function video(key:String)
 	{
-		return Asset2File.getPath('assets/videos/$key.$VIDEO_EXT');
+		return Generic.returnPath() + 'assets/videos/$key.$VIDEO_EXT';
 	}
 
 	static public function sound(key:String, ?library:String):Sound
