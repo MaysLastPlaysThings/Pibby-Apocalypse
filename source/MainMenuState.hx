f0822db67384680f018c56d76905f1419888ab23
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-01-13 14:20:21 +0200

    update source/MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -66,6 +66,9 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		#if MODS_ALLOWED
 		Paths.pushGlobalMods();
 		WeekData.loadTheFirstEnabledMod();
