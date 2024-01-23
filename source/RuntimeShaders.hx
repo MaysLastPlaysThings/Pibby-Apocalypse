02ab18039eedccdf4c467d228d79cf68b7662ab4
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-01-23 12:33:27 +0200

    should fix it

--- source/RuntimeShaders.hx
+++ source/RuntimeShaders.hx
@@ -419,9 +419,9 @@ enum abstract RuntimeShaders(String) to String from String
 
     var pixel = 
     "#pragma header
-	vec2 uv = openfl_TextureCoordv.xy;
-	vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
-	vec2 iResolution = openfl_TextureSize;
+	vec2 uv;
+	vec2 fragCoord;
+	vec2 iResolution;
 	uniform float iTime;
 	#define iChannel0 bitmap
 	#define texture texture2D
@@ -431,6 +431,9 @@ enum abstract RuntimeShaders(String) to String from String
 	uniform float size;
 
 	void mainImage() {
+	  uv = openfl_TextureCoordv.xy;
+	  fragCoord = openfl_TextureCoordv*openfl_TextureSize;
+	  iResolution = openfl_TextureSize;
 		vec2 coordinates = fragCoord.xy/iResolution.xy;
 		vec2 pixelSize = vec2(size/iResolution.x, size/iResolution.y);
 		vec2 position = floor(coordinates/pixelSize)*pixelSize;
